a90a67a1c0155af959da505eeb10c45370f55167
gustavo mena <gustavo.mena@gmail.com>
2022-02-05 22:40:13 -0300

    adding pieces

---
+++ db/db.js
@@ -0,0 +1,139 @@
+const { Pool } = require("pg");
+
+const config = {
+  user: "postgres",
+  host: "localhost",
+  password: "postgres",
+  port: 5432,
+  database: "bancosolar",
+};
+
+const pool = new Pool(config);
+
+function dbError(error, message) {
+  console.log("mensaje:" + message);
+  console.log("database error:" + error.code);
+}
+
+const setUser = async (data) => {
+  const req = {
+    text: "INSERT INTO usuarios(nombre, balance) VALUES ($1, $2)",
+    values: data,
+  };
+  try {
+    const res = await pool.query(req);
+    return res;
+  } catch (error) {
+    dbError(error, "insertando usuarios");
+    return error;
+  }
+};
+const getUsers = async () => {
+  const req = {
+    text: "SELECT * FROM usuarios ORDER BY id",
+  };
+  try {
+    const res = await pool.query(req);
+    return res;
+  } catch (error) {
+    dbError(error, "Obteniendo usuarios");
+    return error;
+  }
+};
+
+const updateUser = async (data, id) => {
+  const req = {
+    text: `UPDATE usuarios SET nombre = $1, balance = $2 WHERE id = ${id} RETURNING *`,
+    values: data,
+  };
+  try {
+    const res = await pool.query(req);
+    return res;
+  } catch (error) {
+    dbError(error, "Actualizando usuarios");
+    return error;
+  }
+};
+
+const deleteUser = async (id) => {
+  const req = {
+    text: `DELETE FROM usuarios WHERE id = ${id}`, //ON DELETE CASCADE ACTIVADO PARA LA REFERENCIA A TRANSFERENCIAS (emisor/receptor)
+  };
+  try {
+    const res = await pool.query(req);
+    return res;
+  } catch (error) {
+      dbError(error,"Borrando usuarios");
+    return error;
+  }
+};
+
+const getTransfers = async () => {
+    const query= "SELECT "
+                +"t.fecha,"
+                +"(SELECT u.nombre FROM usuarios as u  WHERE t.emisor = u.id) as emisor,"
+                +"u.nombre as receptor,"
+                +"t.monto FROM usuarios as u"
+                +"INNER JOIN transferencias as t "
+                +"ON t.receptor = u.id ORDER BY t.id;"
+  const req = {
+    rowMode: "array",
+    text: query,
+  };
+
+  try {
+    const res = await pool.query(req);
+    return res;
+  } catch (error) {
+    dbError(error,"consultando transferencias");
+    return error;
+  }
+};
+const setTransfer = async (data) => {
+    try {
+      await pool.query("BEGIN");
+      const discount = {
+        text: `UPDATE usuarios SET balance = balance - ${data[2]} WHERE nombre = '${data[0]}' RETURNING*`,
+      };
+      const resDiscount = await pool.query(discount);
+      
+      const credit = {
+        text: `UPDATE usuarios SET balance = balance + ${data[2]} WHERE nombre = '${data[1]}' RETURNING*`,
+      };
+      const resCredit = await pool.query(credit);
+  
+      const logTransfer = {
+        text: "INSERT INTO transferencias(emisor, receptor, monto, fecha) VALUES($1, $2, $3, $4)",
+        values: [
+          resDiscount.rows[0].id,
+          resCredit.rows[0].id,
+          data[2],
+          new Date(),
+        ],
+      };
+      await pool.query(logTransfer);
+      await pool.query("COMMIT");
+      
+      const data = [
+        resDiscount.rows[0].nombre,
+        acreditacion.rows[0].nombre,
+        data[2],
+        new Date(),
+      ];
+      return data;
+    } catch (error) {
+      await pool.query("ROLLBACK");
+  dbError(error, "Procesando una transferencia");
+      return error;
+    }
+  };
+  
+
+module.exports = {
+  setUser,
+  getUsers,
+  updateUser,
+  deleteUser,
+  getTransfers,
+  setTransfer
+};
---
+++ private/index.js
@@ -0,0 +1,140 @@
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+const dbAction= require("../db/db");
+
+
+
+http.createServer(
+async (req, res) =>{
+    
+    if (req.url == "/" && req.method === "GET") {
+        try {
+          res.setHeader("Content-Type", "text/html");
+          const html = fs.readFileSync("public/index.html", "utf-8");
+          res.statusCode = 200;
+          res.end(html);
+          
+        } catch (error) {
+          console.log("catch error:", error.code);
+          const html = fs.readFileSync("errors/404.html", "utf-8");
+          res.statusCode = 404;
+          res.end(html);          
+        }
+      }
+//leer usuarios
+      if (req.url == "/usuarios" && req.method === "GET") {
+        try {
+          const req = await dbAction.getUsers();
+          res.statusCode = 200
+          res.end(JSON.stringify(req.rows))
+          
+        } catch (error) {
+          const html = fs.readFileSync("errors/404.html", "utf-8");
+          res.statusCode = 404;
+          res.end(html);      
+        }
+      }
+
+ //crear usuario     
+      if (req.url == "/usuario" && req.method === "POST") {
+        try {
+          let body = ""
+          req.on("data", (chunk) => {
+            body += chunk
+          })
+          req.on("end", async () => {
+            const data = Object.values(JSON.parse(body))
+            const dbResponse = await dbAction.setUser(data);
+            res.statusCode = 201;
+            res.end(JSON.stringify(dbResponse));
+          });
+          
+        } catch (error) {
+          const html = fs.readFileSync("errors/error.html", "utf-8");
+          res.statusCode = 400;
+          res.end(html);
+          
+        }
+      }
+
+//editar usuario
+      if (req.url.startsWith("/usuario") && req.method === "PUT") {
+        const { id } = url.parse(req.url, true).query;
+  
+        try {
+          let body = ""
+          req.on("data", (chunk) => {
+            body += chunk
+          })
+    
+          req.on("end", async () => {
+            const data = Object.values(JSON.parse(body))
+            const dbResponse = await dbAction.updateUser(data, id);
+            res.statusCode = 201;
+            res.end(JSON.stringify(dbResponse));
+          });
+          
+        } catch (error) {
+          const html = fs.readFileSync("errors/error.html", "utf-8");
+          res.statusCode = 400;
+          res.end(html);
+        }
+      }
+
+      if (req.url.startsWith("/usuario") && req.method === "DELETE") {
+        const { id } = url.parse(req.url, true).query;
+        try {
+          const dbResponse = await dbAction.deleteUser(id);
+          res.statusCode = 202;
+          res.end(JSON.stringify(dbResponse));
+          
+        } catch (error) {
+          const html = fs.readFileSync("errors/error.html", "utf-8");
+          res.statusCode = 400;
+          res.end(html);
+        }
+      }
+
+      if (req.url == "/transferencia" && req.method === "POST") {
+        try {
+          let body = ""
+          req.on("data", (chunk) => {
+            body += chunk
+          })
+    
+          req.on("end", async () => {
+            const data = Object.values(JSON.parse(body))
+            const dbResponse = await dbAction.setTransfer(data);
+            res.statusCode = 201;
+            res.end(JSON.stringify(dbResponse));
+          })
+          
+        } catch (error) {
+          const html = fs.readFileSync("errors/error.html", "utf-8");
+          res.statusCode = 400;
+          res.end(html);
+        }
+      }
+
+
+      if (req.url == "/transferencias" && req.method === "GET") {
+        try {
+          const dbResponse = await dbAction.getTransfers();
+          res.statusCode = 200;
+          res.end(JSON.stringify(dbResponse.rows));
+          
+        } catch (error) {
+          const html = fs.readFileSync("errors/404.html", "utf-8");
+          res.statusCode = 404;
+          res.end(html);
+          
+        }
+      }
+  
+
+}
+
+
+).listen(3000,()=>console.log('UP! no errors'));
+
